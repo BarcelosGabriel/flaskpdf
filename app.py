60437957bbb55c0a5df43617d637e73b91a01fef
Gabriel Barcelos <85522842+BarcelosGabriel@users.noreply.github.com>
2024-07-25 22:41:30 -0300

    firts comiit

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/')
 def index():
     return render_template('upload.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
+def upload():
     if 'pdf_file' not in request.files:
-        return 'No file part'
+        return "Nenhum arquivo PDF enviado!"
+    
     file = request.files['pdf_file']
     if file.filename == '':
-        return 'No selected file'
+        return "Nenhum arquivo selecionado!"
+    
     if file and file.filename.endswith('.pdf'):
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File successfully uploaded'
+        # Salva o arquivo ou faz o processamento necessário
+        return "Arquivo recebido com sucesso!"
     else:
-        return 'Invalid file format. Only PDFs are allowed.'
+        return "Apenas arquivos PDF são permitidos!"
 
 if __name__ == '__main__':
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
     app.run(debug=True)
